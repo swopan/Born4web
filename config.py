6cb4ea09a4cfdf35ffda048f29fad3db52a52a57
swopan <38856499+swopan@users.noreply.github.com>
2024-03-19 13:15:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1040980316"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://swopandeep7:Batala321@swopan.44qqnn7.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Swopan:Batala123@swopan.jpnjto6.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Swopan")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "upshrink.com")
