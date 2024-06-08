b5282d54d60d69bf61b8a1f3118a2dbbc12c93f3
swopan <38856499+swopan@users.noreply.github.com>
2024-06-08 12:50:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "upshrink.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "455fbdb800c013ddec4027ab873446ccd5bdef5b")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","backup9910/16")
+TUT_VID = os.environ.get("TUT_VID","tutorialimks/6")
 
 
 #force sub channel id, if you want enable force sub
